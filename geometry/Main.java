85395facb89d1469cc926299ba42b072be1009b8
Artur <artur.pawelec@microsoft.wsei.edu.pl>
2024-04-07 12:08:44 +0200

    zad2

--- geometry/Main.java
+++ geometry/Main.java
@@ -1,17 +1,15 @@
+package geometry;
+
 public class Main {
 
     public static void main(String[] args) {
-        Point punkt = new Point(2.5, 3.14);
+        Point center = new Point(2.5, 3.14);
+        Circle circle = new Circle(center, 5.0);
 
-        System.out.println("Współrzędne punktu:");
-        System.out.println("x = " + punkt.getX());
-        System.out.println("y = " + punkt.getY());
-
-        punkt.setX(10.0);
-        punkt.setY(5.5);
-
-        System.out.println("\nNowe współrzędne punktu:");
-        System.out.println("x = " + punkt.getX());
-        System.out.println("y = " + punkt.getY());
+        System.out.println("Właściwości koła:");
+        System.out.println("Środek: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Promień: " + circle.getRadius());
+        System.out.println("Obwód: " + circle.calculatePerimeter());
+        System.out.println("Pole powierzchni: " + circle.getArea());
     }
 }
