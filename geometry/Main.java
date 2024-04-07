47c192fb038ae3cb3e81811a92111b14a46dda91
Artur <artur.pawelec@microsoft.wsei.edu.pl>
2024-04-07 12:33:38 +0200

    zad6

--- geometry/Main.java
+++ geometry/Main.java
@@ -4,18 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Circle[] circles = new Circle[3];
+        Square kwadrat = new Square(4.7);
 
-        circles[0] = new Circle(new Point(1.0, 2.0), 3.0);
-        circles[1] = new ColoredCircle(new Point(4.0, 5.0), 6.0, "Niebieski");
-        circles[2] = new Circle(new Point(7.0, 8.0), 9.0);
-
-        for (Circle circle : circles) {
-            System.out.println("Pole powierzchni: " + circle.calculateArea());
-            if (circle instanceof ColoredCircle) {
-                ColoredCircle coloredCircle = (ColoredCircle) circle;
-                System.out.println("Kolor: " + coloredCircle.getColor());
-            }
-        }
+        System.out.println("Właściwości kwadratu:");
+        System.out.println("Bok: " + kwadrat.length);
+        System.out.println("Pole powierzchni: " + kwadrat.calculateArea());
+        System.out.println("Obwód: " + kwadrat.calculatePerimeter());
     }
 }
---
+++ geometry/Square.java
@@ -0,0 +1,10 @@
+package geometry;
+
+public class Square extends Rectangle {
+
+    public String length;
+
+    public Square(double side) {
+        super(side, side);
+    }
+}
